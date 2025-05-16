eea3711c25b00948a6271ff54364376209aae2f4
gianmalfate <gianmalfate@gmail.com>
2025-05-16 18:07:33 -0300

    Remove SECRET_KEY from settings and load from .env

--- dia_organizado/settings.py
+++ dia_organizado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xs#0%1oj8bo2n3!7v#cn$cpg0xeel4ofu0s*ls40w06s$%s#($"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
